589f0e0c43269921d0953ae674286339a9c7c9e0
Mobil Game 06 <75528709+MobilGame06@users.noreply.github.com>
2021-06-19 12:56:50 +0200

    Update program_list.lua

--- program_list.lua
+++ program_list.lua
@@ -4,4 +4,8 @@ progs.test = {}
 progs.test.id = "https://raw.githubusercontent.com/MobilGame06/Opencompuers-Hub-Installer/main/programs/test.lua"
 progs.test.file = "/home/lol.lua"
 
+progs.nfcwriter = {}
+progs.nfcwriter.id = "https://github.com/MobilGame06/opencomputers-NFC-Tools/blob/main/write.lua"
+progs.nfcwriter.file = "/usr/nfcwriter.lua"
+
 return progs
