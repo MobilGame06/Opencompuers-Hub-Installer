88e50d45e505e8051147c1ab9baecf10c98d30a9
Mobil Game 06 <75528709+MobilGame06@users.noreply.github.com>
2021-06-19 12:07:35 +0200

    Update program_list.lua

--- program_list.lua
+++ program_list.lua
@@ -1,1 +1,19 @@
-
+local progs = {}
+ 
+progs.graphing = {}
+progs.graphing.id = "yBXSK095"
+progs.graphing.file = "/lib/graphing.lua"
+ 
+progs.installer = {}
+progs.installer.id = "GiZ18Hk9"
+progs.installer.file = "/bin/brainInstaller.lua"
+ 
+progs.inductiondisplay = {}
+progs.inductiondisplay.id = "tPcLtTZH"
+progs.inductiondisplay.file = "/bin/inductionDisplay.lua"
+ 
+progs.reactorcontrol = {}
+progs.reactorcontrol.id = "YjP1yCBn"
+progs.reactorcontrol.file = "/bin/reactorControl.lua"
+ 
+return progs
