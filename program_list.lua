f098d4a949a711bea803a868978e79fea7ade60b
Mobil Game 06 <75528709+MobilGame06@users.noreply.github.com>
2021-08-01 16:55:34 +0200

    Update program_list.lua

--- program_list.lua
+++ program_list.lua
@@ -8,5 +8,8 @@ progs.nfcwriter = {}
 progs.nfcwriter.id = "https://raw.githubusercontent.com/MobilGame06/Opencomputers-Hub-Installer/main/programs/nfcwriter.lua"
 progs.nfcwriter.file = "/usr/bin/nfcwriter.lua"
 
+progs.nfcreader = {}
+progs.nfcreader.id = "https://raw.githubusercontent.com/MobilGame06/Opencomputers-Hub-Installer/main/programs/write.lua"
+progs.nfcreader.file = "/usr/bin/nfcreader.lua"
 
 return progs
