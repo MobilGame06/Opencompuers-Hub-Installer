ca1c8d85fb27b03bffc93164510ec594830df7fa
Mobil Game 06 <nico.schnass@gmail.com>
2022-10-06 16:41:00 +0200

    Create installerCmd.lua

---
+++ installerCmd.lua
@@ -0,0 +1,35 @@
+local comp = require("component")
+local term = require("term")
+local computer = require("computer")
+local shell = require("shell")
+local text = require("text")
+
+package.loaded.BrainInstallerList = nil
+os.remove("/lib/OpencomputersHubInstallerList.lua")
+computer.beep(850,0.15)
+computer.beep(1000,0.08)
+os.execute("wget 'https://raw.githubusercontent.com/MobilGame06/Opencomputers-Hub-Installer/main/program_list.lua' /lib/OpencomputersHubInstallerList.lua")
+package.loaded.BrainInstallerList = nil
+local list = require("OpencomputersHubInstallerList")
+
+local args, options = shell.parse(...)
+options.q = options.q or options.Q
+local prog = text.trim(args[1])
+prog = prog:lower()
+
+print(prog)
+local dl = list[prog]
+
+if not list[prog] then
+    term.write("Unknown packacke \n")
+    computer.beep(1000,0.15)
+    os.sleep(1)
+    os.exit()
+  else
+      term.write("Downloading \n")
+      computer.beep(200,0.08)
+  end
+os.execute("wget '".. dl.id .."' ".. dl.file)
+computer.beep(950,0.08)
+term.clear()
+term.write("Your file has been downloaded to " .. dl.file)
